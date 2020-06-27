0f0786c0bb5bec827ed1b8076a95db3c62b1c113
ant-hunt123 <sarveshraut123@gmail.com>
2020-06-27 19:45:22 +0530

    Updates

---
+++ precache-manifest.7f69cc7b683cba31824f15fc674ad752.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6febb72f4b66ff8df10a29c2b30f7c",
+    "url": "/Covid19.info/index.html"
+  },
+  {
+    "revision": "fcbd5b1b5939e3b18230",
+    "url": "/Covid19.info/static/js/2.38a3b34c.chunk.js"
+  },
+  {
+    "revision": "ab657711d259c70a2ecb",
+    "url": "/Covid19.info/static/js/main.46f751b6.chunk.js"
+  },
+  {
+    "revision": "7803c718b871e33275f6",
+    "url": "/Covid19.info/static/js/runtime-main.6a2881a5.js"
+  }
+]);
